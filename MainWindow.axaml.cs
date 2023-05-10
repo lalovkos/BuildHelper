f7b6d411592c5a5830ac21c1e6b4e3425a05f171
Константин Лалов <lalovkos@gmail.com>
2023-05-10 22:16:55 +0700

    Add project files.

---
+++ MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace BuildHelper
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
